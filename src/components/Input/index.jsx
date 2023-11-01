26c17ddb3eb0808edfd326ab5a0cfa2e2ba8244f
JPabloVM <joaopablo778@gmail.com>
2023-10-31 22:57:10 -0300

    feat/UserRegistrationIntegration

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,22 @@
 import './input.css';
 
-export default function Input({ type, name, id, placeholder, required }) {
+export default function Input({
+  type,
+  name,
+  id,
+  value,
+  onChange,
+  placeholder,
+  required,
+}) {
   return (
     <input
       className="input"
       type={type}
       name={name}
       id={id}
+      value={value}
+      onChange={onChange}
       placeholder={placeholder}
       required={required}
     ></input>
--- src/pages/UserRegistration/index.jsx
+++ src/pages/UserRegistration/index.jsx
@@ -6,30 +6,119 @@ import Button from '../../components/Button';
 import Label from '../../components/Label';
 import SimpleTitle from '../../components/SimpleTitle';
 import './UserRegistration.css';
+import axios from 'axios';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const url = 'http://localhost:8080/4nutri-api/';
 
 export default function UserRegistration() {
+  const [userData, setUserData] = useState({
+    name: '',
+    crn: '',
+    cpf: '',
+    email: '',
+    phone: '',
+    mobile: '',
+    birth_date: '',
+    gender: '',
+    password: '',
+    confirm_password: '',
+  });
+
+  const createUser = async () => {
+    try {
+      const response = await axios.post(url, userData);
+      console.log(response);
+      if (response.data.status == 200) {
+        toast.success('Usuário cadastrado com sucesso!');
+      } else {
+        toast.error('Ocorreu um erro ao cadastrar usuário');
+      }
+    } catch (error) {
+      toast.error('Ocorreu um erro ao cadastrar usuário');
+    }
+  };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const saveUser = (event) => {
+    event.preventDefault();
+
+    createUser();
+  };
+
   return (
     <PageBuilder pageName="Cadastro de Usuário" userName="João Pablo">
       <Form>
         <SimpleTitle>Cadastre-se</SimpleTitle>
 
         <Label>Nome</Label>
-        <Input type="text" name="nome" id="nome"></Input>
+        <Input
+          type="text"
+          name="name"
+          id="name"
+          value={userData.name}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>Email</Label>
-        <Input type="email" name="email" id="email"></Input>
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          value={userData.email}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>CRN</Label>
-        <Input type="text" name="CRN" id="CRN"></Input>
+        <Input
+          type="text"
+          name="crn"
+          id="crn"
+          value={userData.crn}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>CPF</Label>
-        <Input type="text" name="CPF" id="CPF"></Input>
+        <Input
+          type="text"
+          name="cpf"
+          id="cpf"
+          value={userData.cpf}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>Telefone</Label>
-        <Input type="tel" name="Telefone" id="Telefone"></Input>
+        <Input
+          type="text"
+          name="phone"
+          id="phone"
+          value={userData.phone}
+          onChange={handleInputChange}
+        ></Input>
+
+        <Label>Celular</Label>
+        <Input
+          type="text"
+          name="mobile"
+          id="mobile"
+          value={userData.mobile}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>Data de nascimento</Label>
-        <Input type="date" name="nascimento" id="nascimento"></Input>
+        <Input
+          type="date"
+          name="birth_date"
+          id="birth_date"
+          value={userData.birth_date}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>Gênero</Label>
         <fieldset className="fildset">
@@ -43,12 +132,26 @@ export default function UserRegistration() {
         </fieldset>
 
         <Label>Senha</Label>
-        <Input type="password" name="Senha" id="Senha"></Input>
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          value={userData.password}
+          onChange={handleInputChange}
+        ></Input>
 
         <Label>Confirme sua senha</Label>
-        <Input type="password" name="Senha" id="ConfirmarSenha"></Input>
+        <Input
+          type="password"
+          name="confirm_password"
+          id="confirm_password"
+          value={userData.confirm_password}
+          onChange={handleInputChange}
+        ></Input>
 
-        <Button width="300px">Cadastrar</Button>
+        <Button width="300px" onClick={saveUser}>
+          Cadastrar
+        </Button>
       </Form>
     </PageBuilder>
   );
