534cafcc98a2165edce304d12db715b18693469f
Douglas Alves <douglas.alves.angelo@hotmail.com>
2023-11-18 21:33:05 -0300

    refact/Validation

--- src/pages/UserRegistration/index.jsx
+++ src/pages/UserRegistration/index.jsx
@@ -11,6 +11,7 @@ import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import handleInputChange from '../../handlers/input.handler';
+import { differenceInYears, parseISO } from 'date-fns';
 
 export default function UserRegistration() {
   const navigate = useNavigate();
@@ -24,7 +25,23 @@ export default function UserRegistration() {
     birth_date: '',
     gender: '',
     password: '',
+    confirm_password: '',
   });
+
+  const [status, setStatus] = useState({
+    type: '',
+    mensagem: '',
+  });
+
+  const validateBirthDate = (birthDate) => {
+    const currentDate = new Date();
+    const parsedBirthDate = parseISO(birthDate);
+    const age = differenceInYears(currentDate, parsedBirthDate);
+    if (age < 18) {
+      return false;
+    }
+    return true;
+  };
 
   const createNutritionist = async () => {
     await axios
@@ -48,13 +65,127 @@ export default function UserRegistration() {
 
   const saveUser = (event) => {
     event.preventDefault();
+    if (!validate()) return;
     createNutritionist();
   };
+
+  const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+  const validEmail = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+  );
+
+  function validate() {
+    if (!userData.name)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo nome!',
+      });
+    if (!userData.email)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo email!',
+      });
+    if (!validEmail.test(userData.email)) {
+      return setStatus({
+        type: 'error',
+        mensagem: 'Email invalido',
+      });
+    }
+    if (!userData.crn)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo CRN!',
+      });
+    if (userData.crn.length < 11 || userData.crn.length > 11)
+      return setStatus({
+        type: 'error',
+        mensagem: 'CRN invalido!',
+      });
+    if (!userData.cpf)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo CPF!',
+      });
+    if (userData.cpf.length < 11 || userData.cpf.length > 11)
+      return setStatus({
+        type: 'error',
+        mensagem: 'CPF invalido!',
+      });
+    if (!userData.phone)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo telefone!',
+      });
+    if (!userData.mobile)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo celular!',
+      });
+    if (userData.mobile.length < 11 || userData.mobile.length > 11)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Número invalido!',
+      });
+    if (!userData.birth_date)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo data de nascimento!',
+      });
+    if (!validateBirthDate(userData.birth_date)) {
+      return setStatus({
+        type: 'error',
+        mensagem: 'Data invalida!',
+      });
+    }
+    if (!userData.gender)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario selecionar o seu genero!',
+      });
+    if (!userData.password)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo senha!',
+      });
+    if (!validPassword.test(userData.password)) {
+      return setStatus({
+        type: 'error',
+        mensagem:
+          'A senha deve conter letras, número e ter no mínimo 6 caracteres!',
+      });
+    }
+    if (!userData.confirm_password)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Necessario preencher o campo confirmar senha!',
+      });
+    if (userData.confirm_password !== userData.password)
+      return setStatus({
+        type: 'error',
+        mensagem: 'Senhas diferentes!',
+      });
+
+    return true;
+  }
 
   return (
     <PageBuilder pageName="Cadastro de Usuário" userName="João Pablo">
       <Form>
         <SimpleTitle>Cadastre-se</SimpleTitle>
+        {status.type === 'success' ? (
+          <p style={{ color: 'green', fontSize: '20px', margin: '10px' }}>
+            {status.mensagem}
+          </p>
+        ) : (
+          ''
+        )}
+        {status.type === 'error' ? (
+          <p style={{ color: '#ff0000', fontSize: '20px', margin: '10px' }}>
+            {status.mensagem}
+          </p>
+        ) : (
+          ''
+        )}
 
         <Label>Nome</Label>
         <Input
@@ -165,7 +296,7 @@ export default function UserRegistration() {
           name="confirm_password"
           id="confirm_password"
           value={userData.confirm_password}
-          // onChange={(event) => handleInputChange(event, setUserData)}
+          onChange={(event) => handleInputChange(event, setUserData)}
         ></Input>
 
         <Button width="300px" onClick={saveUser}>
